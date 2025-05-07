1af2dfda507b6db6f7854bbc7c5fa0cb06bf41fa
zaknisdev <zakianisf1710@gmail.com>
2025-05-07 19:41:29 +0700

    membuat function untuk bubble sort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -31,3 +31,21 @@ void input()
         cin >> elemen[i];
     }
 }
+
+void bubbleSortArray()
+{
+    int pass = 1;
+    do
+    {
+        for (int i = 0; i <= n - pass - 1; i++)
+        {
+            if (elemen[i] > elemen[i + 1])
+            {
+                int temp = elemen[i];
+                elemen[i] = elemen[i + 1];
+                elemen[i + 1] = temp;
+            }
+        }
+        pass++;
+    } while (pass < n);
+}
