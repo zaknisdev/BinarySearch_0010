a7a08b7d06fcaffaa668afd92bd77dbb051a41d3
zaknisdev <zakianisf1710@gmail.com>
2025-05-07 19:40:46 +0700

    membuat perulangan for

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -24,4 +24,10 @@ void input()
     cout << "==============================" << endl;
     cout << "Masukkan Elemen Array" << endl;
     cout << "==============================" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> elemen[i];
+    }
 }
